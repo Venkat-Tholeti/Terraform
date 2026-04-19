c071bda9a8cc9c3973e2fbacca24974a93cb2c0a
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-19 11:48:52 +0530

    Terraform Provider

---
+++ Ec2.tf
@@ -0,0 +1,26 @@
+
+resource "aws_security_group" "test" {
+      name = "test_group"
+      description = "Practice concept"
+
+      ingress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+      }
+      
+      egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+      }
+
+      tags = {
+            Name = "Testing"
+            Project = "Terraform"
+      }
+}
